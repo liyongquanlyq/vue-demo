b88c419c2f1c250344c42a7a9a120435d7e4dbee
lisafra <1060796977@qq.com>
2017-04-01 12:57:57 +0800

    修改彩友圈bug

--- src/views/home/LiveCircle.vue
+++ src/views/home/LiveCircle.vue
@@ -3,7 +3,7 @@
     <x-header :left-options="{showBack:false}">彩友圈</x-header>
     <scroller lock-x height="-99px" ref="ScrollerDom">
       <div>
-        我是彩友圈...
+        我是彩友圈,
         我再加点字...
       </div>
     </scroller>
